06cb6f0fd8e01f97e102dfa55f783d3915fe6683
張建文 <chang180@gmail.com>
2020-07-31 14:29:30 +0800

    顯示全部商品

--- front/main.php
+++ front/main.php
@@ -1,1 +1,22 @@
-Hello World!!
+<h1>全部商品</h1>
+<?php
+$goods=$Goods->all(['sh'=>1]);
+
+foreach($goods as $g){
+    ?>
+<div style="display:flex;width:80%">
+<div style="width:40%"><img src="img/<?=$g['img'];?>" style="width:80%"></div>
+<div style="width:60%">
+<div><?=$g['name'];?></div>
+<div>價格：<?=$g['price'];?>
+<a href="?do=login"><img src="img/0402.jpg" style="float:right"></a></div>
+<div>規格：<?=$g['spec'];?></div>
+<div>簡介<?=mb_substr($g['intro'],0,25,"utf8");?>...</div>
+</div>
+
+</div>
+<hr>
+    <?php
+}
+
+?>
